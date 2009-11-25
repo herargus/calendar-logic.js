15b0bf75566596d80f265783c8f55f4c16d15920
August Lilleaas <augustlilleaas@gmail.com>
2009-11-25 22:43:35 +0100

    Right, the tests.

--- test/tests.js
+++ test/tests.js
@@ -1,21 +1,21 @@
 (function () {
-  TestCase("JsLowlevelCalendar", {
+  TestCase("JsCalendarLogic", {
     "test creation of day names": function () {
-      c = new LowlevelCalendar();
+      c = new CalendarLogic();
       assertEquals("Sunday", c.options.dayNames[0]);
       assertEquals("Saturday", c.options.dayNames[6]);
 
-      var c = new LowlevelCalendar({firstDayOfWeek: 2});
+      var c = new CalendarLogic({firstDayOfWeek: 2});
       assertEquals("Monday", c.options.dayNames[0]);
       assertEquals("Sunday", c.options.dayNames[6]);
 
-      var c = new LowlevelCalendar({firstDayOfWeek: 5});
+      var c = new CalendarLogic({firstDayOfWeek: 5});
       assertEquals("Thursday", c.options.dayNames[0]);
       assertEquals("Wednesday", c.options.dayNames[6]);
     },
     
     "test incrementing and decrementing": function () {
-      c = new LowlevelCalendar({now: new Time(2008, 5, 17)});
+      c = new CalendarLogic({now: new Time(2008, 5, 17)});
 
       c.incrementMonth();
       assertEquals(2, c.months.length);
@@ -40,17 +40,17 @@
     },
     
     "test creation of cells": function () {
-      c = new LowlevelCalendar({now: new Time(2008, 5, 17)});
+      c = new CalendarLogic({now: new Time(2008, 5, 17)});
       assertEquals(new Time(2008, 4, 27).epoch(), c.months[0].cells[0][0].time.epoch());
       assertEquals(new Time(2008, 5, 1).epoch(), c.months[0].cells[0][4].time.epoch());
 
-      c = new LowlevelCalendar({now: new Time(2008, 5, 17), firstDayOfWeek: 2});
+      c = new CalendarLogic({now: new Time(2008, 5, 17), firstDayOfWeek: 2});
       assertEquals(new Time(2008, 4, 28).epoch(), c.months[0].cells[0][0].time.epoch())
       assertEquals(new Time(2008, 5, 1).epoch(), c.months[0].cells[0][3].time.epoch())
     },
     
     "test offdays": function () {
-      c = new LowlevelCalendar({now: new Time(2008, 5, 17)});
+      c = new CalendarLogic({now: new Time(2008, 5, 17)});
       m = c.months[0];
       assertTrue(c.months[0].cells[0][0].isOffday)
       assertTrue(c.months[0].cells[0][1].isOffday)
@@ -58,7 +58,7 @@
       assertTrue(c.months[0].cells[0][3].isOffday)
       assertFalse(c.months[0].cells[0][4].isOffday)
 
-      c = new LowlevelCalendar({now: new Time(2008, 5, 17), firstDayOfWeek: 2})
+      c = new CalendarLogic({now: new Time(2008, 5, 17), firstDayOfWeek: 2})
       m = c.months[0];
 
       assertTrue(c.months[0].cells[0][0].isOffday)
@@ -69,7 +69,7 @@
     
     "test isToday": function () {
       var now = new Time();
-      c = new LowlevelCalendar({now: now});
+      c = new CalendarLogic({now: now});
       var cellForToday = c.months[0].cells[now.weekOfCurrentMonth() - 1][now.weekday() - 1];
       
       // Testing that it is in fact today
