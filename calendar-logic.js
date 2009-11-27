c06d5c96b17d760351bee16d71fe41f79ba1cce3
August Lilleaas <augustlilleaas@gmail.com>
2009-11-27 18:43:35 +0100

    Using foo.prototype = {func, func} instead of foo.prototype.foo = func, foo.prototype.bar = func.

--- calendar-logic.js
+++ calendar-logic.js
@@ -23,41 +23,42 @@
 		this.currentMonthIndex  = 0;
 	}
 	
-  // An ordered version of options.dayNames.
-	CalendarLogic.prototype.sortDayNames = function(){
-		var tail      = [];
-		var counter   = this.options.firstDayOfWeek;
-		
-		while (counter > 1) {
-			tail.push(this.options.dayNames.shift());
-			counter--;
-		}
-    
-    for (var i = 0; i < tail.length; i ++) {
-      this.options.dayNames.push(tail[i]);
-    }
-	}
-	
-	CalendarLogic.prototype.decrementMonth = function(){
-    this.options.now.advanceMonths(-1)
-    
-    if (this.currentMonthIndex == 0) {
-       this.currentMonth = new CalendarLogic.Month(this);
-       this.months.unshift(this.currentMonth);
-    } else {
-      this.currentMonthIndex--;
-      this.currentMonth = this.months[this.currentMonthIndex];
-    }
-	}
-	
-	CalendarLogic.prototype.incrementMonth = function(){
-		this.options.now.advanceMonths(1)
-		this.currentMonthIndex++;
-		
-		if (!this.months[this.currentMonthIndex]) {
-      this.currentMonth = new CalendarLogic.Month(this);
-      this.months.push(this.currentMonth);
-		}
+	CalendarLogic.prototype = {
+	  sortDayNames: function () {
+	    var tail      = [];
+  		var counter   = this.options.firstDayOfWeek;
+
+  		while (counter > 1) {
+  			tail.push(this.options.dayNames.shift());
+  			counter--;
+  		}
+
+      for (var i = 0; i < tail.length; i ++) {
+        this.options.dayNames.push(tail[i]);
+      }
+	  },
+	  
+	  decrementMonth: function () {
+	    this.options.now.advanceMonths(-1)
+
+      if (this.currentMonthIndex == 0) {
+         this.currentMonth = new CalendarLogic.Month(this);
+         this.months.unshift(this.currentMonth);
+      } else {
+        this.currentMonthIndex--;
+        this.currentMonth = this.months[this.currentMonthIndex];
+      }
+	  },
+	  
+	  incrementMonth: function () {
+	    this.options.now.advanceMonths(1)
+  		this.currentMonthIndex++;
+
+  		if (!this.months[this.currentMonthIndex]) {
+        this.currentMonth = new CalendarLogic.Month(this);
+        this.months.push(this.currentMonth);
+  		}
+	  }
 	}
 	
 	CalendarLogic.Month = function(calendar){
@@ -69,26 +70,28 @@
 		this.generateCells();
 	}
 	
-	CalendarLogic.Month.prototype.generateCells = function(){
-		var week          = 0;
-		var timeInstance  = this.time.clone().firstDayInCalendarMonth()
-		timeInstance.advanceDays(-1);
-		
-		var weeksInMonth = this.time.weeksInMonth()
-		while (week < weeksInMonth) {
-			var cellsThisWeek   = [];
-			var day             = 0;
-			
-			while (day < 7) {
-			  timeInstance.advanceDays(1);
-				var cell = new CalendarLogic.Month.Cell(this, timeInstance);
-				this.calendar.options.cellCallback(timeInstance);
-				cellsThisWeek.push(cell);
-				day++;
-			}
-			this.cells.push(cellsThisWeek);
-			week++;
-		}
+	CalendarLogic.Month.prototype = {
+	  generateCells: function () {
+	    var week          = 0;
+  		var timeInstance  = this.time.clone().firstDayInCalendarMonth()
+  		timeInstance.advanceDays(-1);
+
+  		var weeksInMonth = this.time.weeksInMonth()
+  		while (week < weeksInMonth) {
+  			var cellsThisWeek   = [];
+  			var day             = 0;
+
+  			while (day < 7) {
+  			  timeInstance.advanceDays(1);
+  				var cell = new CalendarLogic.Month.Cell(this, timeInstance);
+  				this.calendar.options.cellCallback(timeInstance);
+  				cellsThisWeek.push(cell);
+  				day++;
+  			}
+  			this.cells.push(cellsThisWeek);
+  			week++;
+  		}
+	  }
 	}
 	
 	CalendarLogic.Month.Cell = function(month, time){
